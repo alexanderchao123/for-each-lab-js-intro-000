92c501cd423d0a44159020a13a901e6574bcccc0
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-28 19:33:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function iterativeLog(array) {
 
 function iterate(callback) {
   var fruits = ["watermelon", "apple", "peach", "pear"]
-  fruits.forEach(callback)
-  return fruits
+  fruits.forEach(callback(element, index, array) {
+    return array
+  })
+  // return fruits
 }
